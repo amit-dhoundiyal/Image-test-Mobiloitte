c8ac498ce4009643d990b6c781d52bbd558c2b7c
amit.dhoundiyal <amitdhoundiyal@mobiloitte.in>
2018-09-11 17:34:30 +0530

    My first commit

--- public/assets/application-517f8c5ee6053607649aa982b2617b8a6e71698acc61f72c5f861a6cb4ef00cf.js
+++ public/assets/application-517f8c5ee6053607649aa982b2617b8a6e71698acc61f72c5f861a6cb4ef00cf.js
@@ -11042,20 +11042,6 @@ return jQuery;
 
 
 
-$( document ).ready(function() {
-       uid = get_uid();
-       $('#img_tag').attr('id',$('#img_tag').attr('id')+uid);
-     // console.log( "ready!" );
-      
-  });
-
-   $(document).on('nested:fieldAdded', function(event){
- // this field was just inserted into your form
- var field = event.field; 
- // it's a jQuery object already! Now you can find date input
- var text_field = field.find('.text');
- // and activate datepicker on it
-});
 (function($) {
   window.NestedFormEvents = function() {
     this.addFields = $.proxy(this.addFields, this);
